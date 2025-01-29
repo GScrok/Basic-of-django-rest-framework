1f9f04b504c1f5f25e2eca2e80ebcd274063717b
GScrok <grmscrok@gmail.com>
2025-01-29 16:41:41 -0300

    ADD: Criação das views Skills e Jobs com ModelViewSet

---
+++ skills/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework import routers
+
+from .views import SkillViewSet
+
+
+app_name = "skills"
+router = routers.SimpleRouter()
+router.register(r"", SkillViewSet, basename="skill")
+
+urlpatterns = router.urls
--- skills/views.py
+++ skills/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Skill
+from .serializers import SkillSerializer
+
+
+class SkillViewSet(ModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
